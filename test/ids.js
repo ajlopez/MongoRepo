5830d2f4ca7eaee7a6e7c76fa5bd964235024c8e
ajlopez <webmaster@ajlopez.com>
2015-12-26 15:31:41 -0300

    String to id, Id to id

--- test/ids.js
+++ test/ids.js
@@ -10,3 +10,17 @@ exports['is id'] = function (test) {
     test.ok(mr.isId(ObjectID.createFromHexString("567ed333a95ac6241543d2d0")));
     test.ok(!mr.isId("z67ed333a95ac6241543d2d0"));
 };
+
+exports['string to id'] = function (test) {
+    var id = mr.toId("567ed333a95ac6241543d2d0");
+    
+    test.equal(typeof id, 'object');
+    test.ok(mr.isId(id));
+};
+
+exports['id to id'] = function (test) {
+    var id = mr.toId("567ed333a95ac6241543d2d0");
+    var id2 = mr.toId(id);
+    
+    test.strictEqual(id2, id);
+};
